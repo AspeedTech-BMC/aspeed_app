AspeedTech-BMC/aspeed_app

commit 040c69dbaf7594d90325705a9050223a0fc32b80

svf: Release the limitation on the amount of TCK.
This patch implements a loop to release the limitation of 255 TCK per
'runtest' command.

Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
Change-Id: I6af5ae724e04bf1ad2663d2ad5c907ac76493df4

svf/ast-jtag.c
@@ -115,18 +115,25 @@ int ast_jtag_run_test_idle(unsigned char end, unsigned int tck)
 {
 	int retval;
 	struct jtag_tap_state run_idle;
+	__u8 execute_tck;
 
-	run_idle.from = JTAG_STATE_CURRENT;
-	run_idle.reset = JTAG_NO_RESET;
-	run_idle.endstate = end;
-	run_idle.tck = tck;
+	while (tck) {
+		execute_tck = tck > 0xff ? 0xff : tck;
+		run_idle.from = JTAG_STATE_CURRENT;
+		run_idle.reset = JTAG_NO_RESET;
+		run_idle.endstate = end;
+		run_idle.tck = execute_tck;
 
-	ast_jtag_printf("from:%d, reset:%d, endstate:%d, tck:%d\n", run_idle.from, run_idle.reset, run_idle.endstate, run_idle.tck);
+		ast_jtag_printf("from:%d, reset:%d, endstate:%d, tck:%d\n",
+				run_idle.from, run_idle.reset,
+				run_idle.endstate, run_idle.tck);
 
-	retval = ioctl(jtag_fd, JTAG_SIOCSTATE, &run_idle);
-	if (retval == -1) {
-		perror("ioctl JTAG run reset fail!\n");
-		return -1;
+		retval = ioctl(jtag_fd, JTAG_SIOCSTATE, &run_idle);
+		if (retval == -1) {
+			perror("ioctl JTAG run reset fail!\n");
+			return -1;
+		}
+		tck -= execute_tck;
 	}
 
 //	if(end)

commit bebd5c3b8135337257f809d6b43d4cb06231da8d

svf: Fix the issue of ignoring the run test command when tck==0.
To avoid the command being ignored due to the TCK being set to 0, this
patch uses the 'do-while' loop.

Fixes: 040c69dbaf759 ("svf: Release the limitation on the amount of TCK.")
Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
Change-Id: I7fabe3bca26211d0f002823cd66523e20a300394

svf/ast-jtag.c
@@ -117,7 +117,7 @@ int ast_jtag_run_test_idle(unsigned char end, unsigned int tck)
 	struct jtag_tap_state run_idle;
 	__u8 execute_tck;
 
-	while (tck) {
+	do {
 		execute_tck = tck > 0xff ? 0xff : tck;
 		run_idle.from = JTAG_STATE_CURRENT;
 		run_idle.reset = JTAG_NO_RESET;
@@ -134,7 +134,7 @@ int ast_jtag_run_test_idle(unsigned char end, unsigned int tck)
 			return -1;
 		}
 		tck -= execute_tck;
-	}
+	} while (tck);
 
 //	if(end)
 //		usleep(3000);